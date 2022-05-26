35641b0b53605c8c057e652ba3507a99d96622cd
Joshua <84692419+JoshuaGitCode@users.noreply.github.com>
2022-05-25 19:55:17 -0700

    ForReal

---
+++ Project/Form1.Designer.cs
@@ -0,0 +1,331 @@
+﻿namespace Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabDrinks = new System.Windows.Forms.TabControl();
+            this.tabMainMenu = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.cboQntSoda2 = new System.Windows.Forms.ComboBox();
+            this.cboQntSoda1 = new System.Windows.Forms.ComboBox();
+            this.lblPriceSoda2 = new System.Windows.Forms.Label();
+            this.lblTitleSoda2 = new System.Windows.Forms.Label();
+            this.lblSoda2 = new System.Windows.Forms.Button();
+            this.lblPriceSoda1 = new System.Windows.Forms.Label();
+            this.lblTitleSoda1 = new System.Windows.Forms.Label();
+            this.lblSoda1 = new System.Windows.Forms.Button();
+            this.tabControl2 = new System.Windows.Forms.TabControl();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.btnDeleteItem = new System.Windows.Forms.Button();
+            this.btnClearList = new System.Windows.Forms.Button();
+            this.lstItemList = new System.Windows.Forms.ListBox();
+            this.btnCheckOut = new System.Windows.Forms.Button();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.lstPrint = new System.Windows.Forms.ListBox();
+            this.tabDrinks.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabControl2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabDrinks
+            // 
+            this.tabDrinks.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabDrinks.Controls.Add(this.tabMainMenu);
+            this.tabDrinks.Controls.Add(this.tabPage2);
+            this.tabDrinks.Location = new System.Drawing.Point(12, 12);
+            this.tabDrinks.Name = "tabDrinks";
+            this.tabDrinks.SelectedIndex = 0;
+            this.tabDrinks.Size = new System.Drawing.Size(805, 667);
+            this.tabDrinks.TabIndex = 0;
+            // 
+            // tabMainMenu
+            // 
+            this.tabMainMenu.Location = new System.Drawing.Point(4, 22);
+            this.tabMainMenu.Name = "tabMainMenu";
+            this.tabMainMenu.Padding = new System.Windows.Forms.Padding(3);
+            this.tabMainMenu.Size = new System.Drawing.Size(797, 641);
+            this.tabMainMenu.TabIndex = 0;
+            this.tabMainMenu.Text = "Main Menu";
+            this.tabMainMenu.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.cboQntSoda2);
+            this.tabPage2.Controls.Add(this.cboQntSoda1);
+            this.tabPage2.Controls.Add(this.lblPriceSoda2);
+            this.tabPage2.Controls.Add(this.lblTitleSoda2);
+            this.tabPage2.Controls.Add(this.lblSoda2);
+            this.tabPage2.Controls.Add(this.lblPriceSoda1);
+            this.tabPage2.Controls.Add(this.lblTitleSoda1);
+            this.tabPage2.Controls.Add(this.lblSoda1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(797, 641);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // cboQntSoda2
+            // 
+            this.cboQntSoda2.FormattingEnabled = true;
+            this.cboQntSoda2.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.cboQntSoda2.Location = new System.Drawing.Point(89, 309);
+            this.cboQntSoda2.Name = "cboQntSoda2";
+            this.cboQntSoda2.Size = new System.Drawing.Size(72, 21);
+            this.cboQntSoda2.TabIndex = 7;
+            // 
+            // cboQntSoda1
+            // 
+            this.cboQntSoda1.FormattingEnabled = true;
+            this.cboQntSoda1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.cboQntSoda1.Location = new System.Drawing.Point(89, 142);
+            this.cboQntSoda1.Name = "cboQntSoda1";
+            this.cboQntSoda1.Size = new System.Drawing.Size(72, 21);
+            this.cboQntSoda1.TabIndex = 6;
+            // 
+            // lblPriceSoda2
+            // 
+            this.lblPriceSoda2.AutoSize = true;
+            this.lblPriceSoda2.Location = new System.Drawing.Point(55, 312);
+            this.lblPriceSoda2.Name = "lblPriceSoda2";
+            this.lblPriceSoda2.Size = new System.Drawing.Size(28, 13);
+            this.lblPriceSoda2.TabIndex = 5;
+            this.lblPriceSoda2.Text = "3.99";
+            // 
+            // lblTitleSoda2
+            // 
+            this.lblTitleSoda2.AutoSize = true;
+            this.lblTitleSoda2.Location = new System.Drawing.Point(52, 187);
+            this.lblTitleSoda2.Name = "lblTitleSoda2";
+            this.lblTitleSoda2.Size = new System.Drawing.Size(26, 13);
+            this.lblTitleSoda2.TabIndex = 4;
+            this.lblTitleSoda2.Text = "Diet";
+            // 
+            // lblSoda2
+            // 
+            this.lblSoda2.Location = new System.Drawing.Point(46, 203);
+            this.lblSoda2.Name = "lblSoda2";
+            this.lblSoda2.Size = new System.Drawing.Size(115, 102);
+            this.lblSoda2.TabIndex = 3;
+            this.lblSoda2.Text = "button4";
+            this.lblSoda2.UseVisualStyleBackColor = true;
+            this.lblSoda2.Click += new System.EventHandler(this.lblSoda2_Click);
+            // 
+            // lblPriceSoda1
+            // 
+            this.lblPriceSoda1.AutoSize = true;
+            this.lblPriceSoda1.Location = new System.Drawing.Point(50, 145);
+            this.lblPriceSoda1.Name = "lblPriceSoda1";
+            this.lblPriceSoda1.Size = new System.Drawing.Size(28, 13);
+            this.lblPriceSoda1.TabIndex = 2;
+            this.lblPriceSoda1.Text = "2.99";
+            // 
+            // lblTitleSoda1
+            // 
+            this.lblTitleSoda1.AutoSize = true;
+            this.lblTitleSoda1.Location = new System.Drawing.Point(52, 18);
+            this.lblTitleSoda1.Name = "lblTitleSoda1";
+            this.lblTitleSoda1.Size = new System.Drawing.Size(32, 13);
+            this.lblTitleSoda1.TabIndex = 1;
+            this.lblTitleSoda1.Text = "Soda";
+            // 
+            // lblSoda1
+            // 
+            this.lblSoda1.Location = new System.Drawing.Point(46, 34);
+            this.lblSoda1.Name = "lblSoda1";
+            this.lblSoda1.Size = new System.Drawing.Size(115, 102);
+            this.lblSoda1.TabIndex = 0;
+            this.lblSoda1.Text = "button1";
+            this.lblSoda1.UseVisualStyleBackColor = true;
+            this.lblSoda1.Click += new System.EventHandler(this.lblSoda1_Click);
+            // 
+            // tabControl2
+            // 
+            this.tabControl2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl2.Controls.Add(this.tabPage3);
+            this.tabControl2.Controls.Add(this.tabPage4);
+            this.tabControl2.Location = new System.Drawing.Point(819, 12);
+            this.tabControl2.Name = "tabControl2";
+            this.tabControl2.SelectedIndex = 0;
+            this.tabControl2.Size = new System.Drawing.Size(261, 663);
+            this.tabControl2.TabIndex = 1;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.btnDeleteItem);
+            this.tabPage3.Controls.Add(this.btnClearList);
+            this.tabPage3.Controls.Add(this.lstItemList);
+            this.tabPage3.Controls.Add(this.btnCheckOut);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(253, 637);
+            this.tabPage3.TabIndex = 0;
+            this.tabPage3.Text = "Order";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // btnDeleteItem
+            // 
+            this.btnDeleteItem.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnDeleteItem.Location = new System.Drawing.Point(169, 589);
+            this.btnDeleteItem.Name = "btnDeleteItem";
+            this.btnDeleteItem.Size = new System.Drawing.Size(75, 23);
+            this.btnDeleteItem.TabIndex = 3;
+            this.btnDeleteItem.Text = "Delete Item";
+            this.btnDeleteItem.UseVisualStyleBackColor = true;
+            this.btnDeleteItem.Click += new System.EventHandler(this.btnDeleteItem_Click);
+            // 
+            // btnClearList
+            // 
+            this.btnClearList.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnClearList.Location = new System.Drawing.Point(88, 589);
+            this.btnClearList.Name = "btnClearList";
+            this.btnClearList.Size = new System.Drawing.Size(75, 23);
+            this.btnClearList.TabIndex = 2;
+            this.btnClearList.Text = "Clear All";
+            this.btnClearList.UseVisualStyleBackColor = true;
+            this.btnClearList.Click += new System.EventHandler(this.btnClearList_Click);
+            // 
+            // lstItemList
+            // 
+            this.lstItemList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstItemList.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstItemList.FormattingEnabled = true;
+            this.lstItemList.ItemHeight = 18;
+            this.lstItemList.Location = new System.Drawing.Point(7, 7);
+            this.lstItemList.Name = "lstItemList";
+            this.lstItemList.Size = new System.Drawing.Size(240, 562);
+            this.lstItemList.TabIndex = 1;
+            // 
+            // btnCheckOut
+            // 
+            this.btnCheckOut.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnCheckOut.Location = new System.Drawing.Point(7, 589);
+            this.btnCheckOut.Name = "btnCheckOut";
+            this.btnCheckOut.Size = new System.Drawing.Size(75, 23);
+            this.btnCheckOut.TabIndex = 0;
+            this.btnCheckOut.Text = "Check Out\r\n";
+            this.btnCheckOut.UseVisualStyleBackColor = true;
+            this.btnCheckOut.Click += new System.EventHandler(this.btnCheckOut_Click);
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.lstPrint);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(253, 637);
+            this.tabPage4.TabIndex = 1;
+            this.tabPage4.Text = "tabPage4";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // lstPrint
+            // 
+            this.lstPrint.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstPrint.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstPrint.FormattingEnabled = true;
+            this.lstPrint.ItemHeight = 20;
+            this.lstPrint.Location = new System.Drawing.Point(7, 6);
+            this.lstPrint.Name = "lstPrint";
+            this.lstPrint.Size = new System.Drawing.Size(240, 564);
+            this.lstPrint.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1092, 821);
+            this.Controls.Add(this.tabControl2);
+            this.Controls.Add(this.tabDrinks);
+            this.Name = "Form1";
+            this.Text = "a";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.tabDrinks.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabControl2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabDrinks;
+        private System.Windows.Forms.TabPage tabMainMenu;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button lblSoda1;
+        private System.Windows.Forms.TabControl tabControl2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Button btnCheckOut;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.Label lblPriceSoda2;
+        private System.Windows.Forms.Label lblTitleSoda2;
+        private System.Windows.Forms.Button lblSoda2;
+        private System.Windows.Forms.Label lblPriceSoda1;
+        private System.Windows.Forms.Label lblTitleSoda1;
+        private System.Windows.Forms.Button btnDeleteItem;
+        private System.Windows.Forms.Button btnClearList;
+        private System.Windows.Forms.ListBox lstItemList;
+        private System.Windows.Forms.ComboBox cboQntSoda1;
+        private System.Windows.Forms.ComboBox cboQntSoda2;
+        private System.Windows.Forms.ListBox lstPrint;
+    }
+}
+
---
+++ Project/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Project
+{
+    public partial class Form1 : Form
+    {
+        string filePath = "order.txt";
+        string filePathQ = "Q.txt";
+        const int SIZE = 500;
+        const double TAX = 0.095;
+        private int count = 0;
+        private string[] itemsNames = new string[SIZE];
+        private int[] itemsQuantity = new int[SIZE];
+        private Double[] itemPrice = new double[SIZE];
+       
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lblSoda1_Click(object sender, EventArgs e)
+        {
+            if (cboQntSoda1.Text == "")
+                MessageBox.Show("Please Choose Quantity");
+            else
+                lstItemList.Items.Add(lblTitleSoda1.Text + " \t" + lblPriceSoda1.Text + "\t " + cboQntSoda1.Text);
+            // Converts 
+            itemPrice[count] = double.Parse(lblPriceSoda1.Text);
+            itemsQuantity[count] = int.Parse(cboQntSoda1.Text);
+            count++;
+        }
+
+        private void lblSoda2_Click(object sender, EventArgs e)
+        {
+            if (cboQntSoda2.Text == "")
+                MessageBox.Show("Please Choose Quantity");
+            else
+                lstItemList.Items.Add(lblTitleSoda2.Text + " \t" + lblPriceSoda2.Text + "\t " + cboQntSoda2.Text);
+            // Converts
+            itemPrice[count] = double.Parse(lblPriceSoda2.Text);
+            count++;
+        }
+
+        private void btnCheckOut_Click(object sender, EventArgs e)
+        {
+            
+            StreamWriter outFile;
+            StreamReader inFile;
+            lstPrint.Items.Clear();        
+            
+            if (lstItemList.Items.Count == 0) // ERROR CHECHS ITEMS
+                MessageBox.Show("No items, Please Add Items!!");
+            else
+            {
+                outFile = File.CreateText(filePath); // OPENS FILE
+                for (int i = 0; i < lstItemList.Items.Count; i++) // FILLS ARRAY WITH TOTAL LIST ITEMS               
+                {
+                    outFile.WriteLine(itemPrice[i]); // PRICE ONLY
+                    outFile.WriteLine(itemsQuantity[i]);
+                }
+                outFile.Close();// CLOSES FILE
+            }
+
+
+            
+            inFile = new StreamReader(filePath); // READS
+           
+            while (!inFile.EndOfStream) 
+            {
+                itemPrice[count] = double.Parse(inFile.ReadLine()); // Converts Price
+                itemsQuantity[count] = int.Parse(inFile.ReadLine());
+                lstPrint.Items.Add(itemPrice[count]); // Displays for Print
+                
+                count++;                            
+            }
+           
+            inFile.Close();
+            lstPrint.Items.Add(itemPrice[0] * itemsQuantity[0]);
+
+
+
+
+
+        }
+
+        private void btnClearList_Click(object sender, EventArgs e)
+        {
+            lstItemList.Items.Clear();
+        }
+
+        private void btnDeleteItem_Click(object sender, EventArgs e)
+        {
+            if (lstItemList.SelectedIndex > -1)
+                lstItemList.Items.RemoveAt(lstItemList.SelectedIndex);
+        }
+    }
+}
